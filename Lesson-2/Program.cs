fdbb3184f0d166161d205751eeb2d8c226bc3010
Akbar Dzhamilov <akbarshoh-95@mail.ru>
2024-02-29 19:49:17 +0300

    lesson 2 решение

---
+++ Lesson-2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Lesson_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bits bits = new Bits(3);
+            bits[1] = false;
+
+            long num = (long)bits;
+            Console.WriteLine(num);
+            Bits bits2 = num;
+        }
+    }
+}
