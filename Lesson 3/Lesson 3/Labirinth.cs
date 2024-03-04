38405e478f4905ab4c19cd2b4ac07450bb7893cd
Akbar Dzhamilov <akbarshoh-95@mail.ru>
2024-03-04 07:35:08 +0300

    Lesson 3

---
+++ Lesson 3/Lesson 3/Labirinth.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_3
+{
+    internal class Labirinth
+    {
+        public static int HasExit(int startI, int startJ, int[,] labirinth)
+        {
+            Queue<(int, int)> coords = new();
+            int count = 0;
+            if (labirinth[startI, startJ] != 1)
+            {
+                coords.Enqueue((startI, startJ));
+            }
+            while (coords.Count > 0)
+            {
+
+
+                (int i, int j) = coords.Dequeue();
+                if (labirinth[i, j] == 2)
+                    count++;
+
+
+                labirinth[i, j] = 1;
+                if (i - 1 >= 0 && labirinth[i - 1, j] != 1) coords.Enqueue((i - 1, j));
+                if (i + 1 < labirinth.GetLength(0) && labirinth[i + 1, j] != 1) coords.Enqueue((i + 1, j));
+                if (j - 1 >= 0 && labirinth[i, j - 1] != 1) coords.Enqueue((i, j - 1));
+                if (j + 1 < labirinth.GetLength(1) && labirinth[i, j + 1] != 1) coords.Enqueue((i, j + 1));
+            }
+            return count;
+        }
+    }
+}
---
+++ Lesson 3/Lesson 3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lesson_3
+{
+    internal class Program
+    {
+        /* Доработайте приложение поиска пути в лабиринте, но на этот раз вам нужно 
+         * определить сколько всего выходов имеется в лабиринте:
+
+            int[,] labirynth1 = new int[,]
+            {
+            {1, 1, 1, 1, 1, 1, 1 },
+            {1, 0, 0, 0, 0, 0, 1 },
+            {1, 0, 1, 1, 1, 0, 1 },
+            {0, 0, 0, 0, 1, 0, 0 },
+            {1, 1, 0, 0, 1, 1, 1 },
+            {1, 1, 1, 0, 1, 1, 1 },
+            {1, 1, 1, 0, 1, 1, 1 }
+            };
+
+            Сигнатура метода:
+
+            static int HasExit(int startI, int startJ, int[,] l)
+        */
+        static void Main(string[] args)
+        {
+           
+            int[,] labirynth1 = new int[,]
+        {
+            {1, 1, 1, 1, 1, 1, 1 },
+            {1, 0, 0, 2, 0, 0, 1 },
+            {1, 0, 1, 1, 1, 0, 1 },
+            {0, 0, 0, 0, 1, 0, 2 },
+            {1, 1, 0, 2, 1, 1, 1 },
+            {1, 1, 1, 1, 1, 1, 1 },
+            {1, 1, 1, 1, 1, 1, 1 }
+        };
+            Console.WriteLine($"Количество выходов= {Labirinth.HasExit(1, 1, labirynth1)}");
+
+        }
+    }
+}
